ceab5e734ba8a48d7ab158254f631ceb09e32d7f
ahmetz3lka <zelkahm@gmail.com>
2025-03-15 17:02:48 +0300

    base model changes

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+_ = load_dotenv(find_dotenv())
 
 # set your openai and genai api keys in a ".env" file
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 GENAI_API_KEY = os.environ["GENAI_API_KEY"]
 
 # change the model names to the ones you want to use
-OPENAI_LLM = "gpt-4o"
+OPENAI_LLM = "gpt-4o-mini"
 GENAI_LLM = "gemini-2.0-flash-exp"
 THINK_LLM = "gemini-2.0-flash-thinking-exp"
